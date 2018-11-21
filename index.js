2af111f8806f2a82c152b622e2a7c20cc2cfc5a1
bummings <eabummings@gmail.com>
2018-11-21 09:21:47 -0500

    added register route with hashing

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
 
-const db = require('./database/dbConfig.js');
+const db = require("./database/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-  res.send('Its Alive!');
+server.get("/", (req, res) => {
+  res.send("Its Alive!");
+});
+
+server.get("/register", (req, res) => {
+  const credentials = req.body;
+
+  // hash the password
+  const hash = bcrypt.hashSync(credentials.password, 14);
+  credentials.password = hash;
+
+  // then save user
+  db("users")
+    .insert(credentials)
+    .then(ids => {
+      const id = ids[0];
+      res.status(201).json({ newUserId: id });
+    });
 });
 
 // protect this route, only authenticated users should see it
-server.get('/api/users', (req, res) => {
-  db('users')
-    .select('id', 'username')
+server.get("/api/users", (req, res) => {
+  db("users")
+    .select("id", "username")
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
 
-server.listen(3300, () => console.log('\nrunning on port 3300\n'));
+server.listen(3300, () => console.log("\nrunning on port 3300\n"));
